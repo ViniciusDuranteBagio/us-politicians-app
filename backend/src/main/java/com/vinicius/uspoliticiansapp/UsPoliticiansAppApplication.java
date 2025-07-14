9ceb97d08a73b227c9d820998ad4d26bf7b82d4b
Vinicius Durante Bagio <viniciusduranteb@gmail.com>
2025-07-14 07:12:38 -0300

    Feat: Adding Spring Boot project created with Spring Initializr

---
+++ backend/src/main/java/com/vinicius/uspoliticiansapp/UsPoliticiansAppApplication.java
@@ -0,0 +1,13 @@
+package com.vinicius.uspoliticiansapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsPoliticiansAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsPoliticiansAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vinicius/uspoliticiansapp/UsPoliticiansAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinicius.uspoliticiansapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsPoliticiansAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
