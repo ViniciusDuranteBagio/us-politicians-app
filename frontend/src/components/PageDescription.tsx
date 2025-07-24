49ba57dba7f7c0a48011fac6d86ee22c0ab33d22
Vinicius Durante Bagio <viniciusduranteb@gmail.com>
2025-07-24 01:44:25 -0300

    feat: add PageDescription and Page Title component

---
+++ frontend/src/components/PageDescription.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PageDescription: React.FC = () => (
+  <p className="text-lg text-blue-700/80 mb-4">
+    Encontre informações sobre representantes de cada estado americano.
+  </p>
+);
+
+export default PageDescription; 
---
+++ frontend/src/components/PageTitle.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const PageTitle: React.FC = () => (
+  <h1 className="text-4xl md:text-5xl font-extrabold text-blue-900 mb-2 tracking-tight drop-shadow-sm">
+    Políticos dos Estados Unidos
+  </h1>
+);
+
+export default PageTitle; 
