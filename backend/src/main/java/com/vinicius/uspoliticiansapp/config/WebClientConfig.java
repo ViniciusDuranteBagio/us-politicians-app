db51aeeaf0db4d69025c6edf37023937dada6d17
Vinicius Durante Bagio <viniciusduranteb@gmail.com>
2025-07-24 01:18:47 -0300

    fix: retrive api key from .env

--- backend/src/main/java/com/vinicius/uspoliticiansapp/config/WebClientConfig.java
+++ backend/src/main/java/com/vinicius/uspoliticiansapp/config/WebClientConfig.java
@@ -1,14 +1,17 @@
 package com.vinicius.uspoliticiansapp.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
+    @Value("${openstates.api.key}")
+    private String apiKey;
+
     @Bean
     public WebClient openStatesWebClient() {
-        String apiKey = System.getenv("API_KEY");
         return WebClient.builder()
                 .baseUrl("https://v3.openstates.org")
                 .defaultHeader("X-API-KEY", apiKey)
