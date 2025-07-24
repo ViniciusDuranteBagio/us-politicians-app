b87d9022c64189a6f09ce6b4fcea5caca45f27a8
Vinicius Durante Bagio <viniciusduranteb@gmail.com>
2025-07-24 01:43:16 -0300

    Feat: add page parameter to api.ts

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 import type { State } from "../types/State";
 import type { Politician } from "../types/Politician";
+import type { PaginationData } from "../types/Pagination";
 
 const API_BASE_URL = "http://localhost:8080/api";
 
@@ -9,11 +10,18 @@ export async function getStates(): Promise<State[]> {
   return res.data.data;
 }
 
-export async function getPoliticians(stateId: number, party?: string): Promise<Politician[]> {
+export interface PoliticiansResponse {
+  data: Politician[];
+  pagination: PaginationData;
+}
+
+export async function getPoliticians(stateId?: number, party?: string, page?: number): Promise<PoliticiansResponse> {
   const params: any = {};
+  if (stateId) params.state = stateId;
   if (party) params.party = party;
+  if (page) params.currentPage = page;
   const res = await axios.get(`${API_BASE_URL}/politicians`, {
-    params: { state: stateId, ...params }
+    params
   });
-  return res.data.data;  
+  return res.data;
 } 
